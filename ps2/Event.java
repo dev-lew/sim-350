d3a8a9d2840edb3521bf7f17cacbaf9941ed7c40
devlew <lewisd@bu.edu>
2022-02-10 20:21:12 -0500

    Refactor Event for ps2 and add Request class

--- ps2/Event.java
+++ ps2/Event.java
@@ -1,22 +1,24 @@
+package ps2;
+
 class Event {
-    private String type;
-    private double timestamp;
-    private int eventIDA;
-    private int eventIDB;
-    private static int eventCountA = 0;
-    private static int eventCountB = 0;
-
-    Event(String type, double timestamp) {
-        this.type = type;
-        this.timestamp = timestamp;
-
-        if (this.type.equals("A"))
-            eventIDA = eventCountA++;
-        else
-            eventIDB = eventCountB++;
+    enum Type {
+        BIRTH,
+        DEATH,
+        MONITOR
     }
 
-    String getType() {
+    private Type type;
+    private double timestamp;
+    private int eventID;
+    private static int numEvents = 0;
+
+    Event(Type type, double timestamp) {
+        this.type = type;
+        this.timestamp = timestamp;
+        eventID = numEvents++;
+    }
+
+    Type getType() {
         return type;
     }
 
@@ -25,9 +27,6 @@ class Event {
     }
 
     public int getEventID() {
-        if (type.equals("A"))
-            return eventIDA;
-        else
-            return eventIDB;
+        return eventID;
     }
 }
--- ps2/Simulator.java
+++ ps2/Simulator.java
@@ -1,13 +1,49 @@
+package ps2;
+
+import static ps2.Event.Type.*;
+
+import java.util.ArrayDeque;
+
 class Simulator {
     class State {
+        class Request {
+            private double arrivalTime;
+            private double startTime;
+            private double finishTime;
+
+            public void setArrivalTime(double arrivalTime) {
+                this.arrivalTime = arrivalTime;
+            }
+
+            public void setStartTime(double startTime) {
+                this.startTime = startTime;
+            }
+
+            public void setFinishTime(double finishTime) {
+                this.finishTime = finishTime;
+            }
+        }
+
         private int queueLength;
         private double requestTime;
         private double busyTime;
+
+        private ArrayDeque<Request> requestQueue = new ArrayDeque<>();
+
+        void addRequest(Request r) {
+            requestQueue.add(r);
+        }
+
+        void dispatchRequest() {
+            requestQueue.removeLast();
+        }
+
+        public int getQueueLength() {
+            queueLength = requestQueue.size();
+            return queueLength;
+        }
     }
 
-    void simulate(double time) {
-
-    }
 
     State initState() {
         return new State();
@@ -15,9 +51,25 @@ class Simulator {
 
     Timeline initTimeline() {
         Timeline tl =  new Timeline();
+        tl.addToTimeline(new Event(BIRTH, 0.0));
+        tl.addToTimeline(new Event(MONITOR, 0.0));
         return tl;
     }
 
+    void executeEvent(Event e) {
+    }
+
+    void simulate(double simDuration) {
+        State s = initState();
+        Timeline t = initTimeline();
+        double time = 0;
+
+        while (time < simDuration) {
+            Event e = t.popNext();
+            executeEvent(e);
+        }
+
+    }
     public static void main(String[] args) {
         if (args.length != 3)
             throw new IllegalArgumentException("Invalid number of arguments");
